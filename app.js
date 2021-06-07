d72657e82001507e0ed64e2f60ef48dcb101f7e2
ashokkrishanth <82096112+ashokkrishanth@users.noreply.github.com>
2021-06-07 16:24:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,4 @@
 const express = require("express");
-const mysql = require("mysql");
-
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
-const session = require("express-session");
-
-app.use(express.json());
-app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', "*");
-    res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
-app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(
   session({
     key: "userId",
